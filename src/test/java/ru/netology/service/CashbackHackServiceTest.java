2c69885ac63a521c80785cbfc0288d2c08201612
Elen Ismaylova <wonderelen@gmail.com>
2021-01-17 13:28:05 +0300

    fixed

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,32 +1,35 @@
 package ru.netology.service;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @org.junit.Test
     public void shouldReturn1000for0() {
-        assertEquals(service.remain (0),1000);
+        assertEquals(1000, service.remain(0));
     }
 
     @org.junit.Test
     public void shouldReturn100for900() {
-        assertEquals(service.remain(100),900);
+        assertEquals(900, service.remain(100));
     }
 
     @org.junit.Test
     public void shouldReturn50for950() {
-        assertEquals(service.remain(50),950);
+
+        assertEquals(950, service.remain(50));
     }
 
     @org.junit.Test
     public void shouldReturn0For1000() {
-        assertEquals(service.remain(1000), 0);
+
+        assertEquals(0, service.remain(1000));
     }
 
     @org.junit.Test
     public void shouldReturn995For5() {
-        assertEquals(service.remain(5), 995);
+
+        assertEquals(995, service.remain(5));
     }
 }
